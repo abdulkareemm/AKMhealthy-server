7ad146197086e79fec4ec62b5fc1b2d7958d9951
abdulkareemm <eng.kreem.96@gmail.com>
2023-03-11 03:27:02 -0800

    booking appointment

--- controller/Appointment.js
+++ controller/Appointment.js
@@ -1,9 +1,13 @@
 const Appointment = require("../models/Appoinment");
 const UserModel = require("../models/User");
+const moment = require("moment");
 
 module.exports.bookAppointment = async (req, res) => {
   try {
     req.body.status = "pending";
+    req.body.date = moment.utc(req.body.date,"DD-MM-YYYY").toISOString()
+    req.body.time = moment.utc(req.body.time, "HH:mm").toISOString();
+
     const newAppointment = new Appointment(req.body);
     await newAppointment.save();
     const user = await UserModel.findOne({ _id: req.body.doctorInfo.userId });
@@ -22,5 +26,39 @@ module.exports.bookAppointment = async (req, res) => {
     return res
       .status(401)
       .json({ msg: "book appointment failed", success: false });
+  }
+};
+
+module.exports.checkBookingAvilability = async (req, res) => {
+  try {
+
+    const date = moment.utc(req.body.date, "DD-MM-YYYY").toISOString();
+    const fromTime = moment.utc(req.body.time,"HH:mm")
+      .subtract(1, "hours")
+      .toISOString();
+    const toTime = moment.utc(req.body.time,"HH:mm")
+      .add(1, "hours")
+      .toISOString();
+
+    const appointment = await Appointment.find({
+      "$and": [
+        {doctorId: req.body.doctorId},
+        {date:date},
+        {time:{$gte:fromTime,$lte:toTime}}
+      ]
+    });
+    if (appointment.length > 0) {
+      return res
+        .status(200)
+        .json({ msg: "Appointment not available", success: false });
+    }
+    res
+      .status(200)
+      .json({ msg: "You can book appointment", success: true });
+  } catch (err) {
+    console.log(err.message);
+    return res
+      .status(401)
+      .json({ msg: "check book appointment failed", success: false });
   }
 };
--- routes/Appoinment.js
+++ routes/Appoinment.js
@@ -1,8 +1,10 @@
 const router = require("express").Router()
-const { bookAppointment } = require("../controller/Appointment");
+const { bookAppointment, checkBookingAvilability } = require("../controller/Appointment");
 const auth = require("../middleware/auth");
 
 
 router.post("/book-appointment", auth, bookAppointment);
+router.post("/check-booking-avilability", auth, checkBookingAvilability);
+
 
 module.exports = router
